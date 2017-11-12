0f254b7b3a043c2c03ef35b6f10bdfd8c132d16d
Valentina Sánchez Bermúdez <vsanche1@eafit.edu.co>
2017-11-12 18:26:59 -0500

    Update GI-add.py

--- GI-add.py
+++ GI-add.py
@@ -1,41 +1,33 @@
 #! /usr/bin/env python3
 
 import os
-import os.path
 import sys
-from Bio import Entrez
 from Bio import SeqIO
 
-fnas = sys.argv[1:]
+files = sys.argv[1]
 
-Entrez.email = 'vsanche1@eafit.edu.co'
+if os.path.exists('GI.txt'):
+    with open('GI.txt', 'r') as gi_file:
+        gi = (gi_file.read()[2:-2].split("', '"))
 
-search_phrase = 'Rodentia[organism:exp]'
-
-handle = Entrez.esearch(db='nuccore', term=search_phrase, retmax=500,
-         rettype='gbwithparts', retmode='text', idtype='GI')
-gi_result = Entrez.read(handle, validate = False)
-gi = list(gi_result['IdList'])
-handle.close()
-print('Done with GI')
-
-handle2 = Entrez.esearch(db='nuccore', term=search_phrase, retmax=500,
-         rettype='gbwithparts', retmode='text', idtype='acc')
-acc_result = Entrez.read(handle2, validate = False)
-acc = list(acc_result['IdList'])
-handle.close()
-print('Done with accession numbers,', 'NM_001357748.1' in acc)
+if os.path.exists('acc.txt'):
+    with open('acc.txt', 'r') as acc_file:
+        acc = (acc_file.read()[2:-2].split("', '"))
 
 # from: NM_001357748.1 Mus musculus coatomer protein complex
 # to:   gi|1274096202|ref|NM_001357748.1| Mus musculus coatomer protein complex
-for fna in fnas:
+for fna in os.listdir(files):
     try:
-        ind = acc.index(fna[:-4])
-        with open(fna, 'rU') as handle:
+        if fna.endswith('.fna'):
+            ind = acc.index(fna[:-4])
+        if fna.endswith('.fasta'):
+            ind = acc.index(fna[:-6])
+
+        with open(files + '/' + fna, 'rU') as handle:
             seq = SeqIO.read(handle, 'fasta')
-            print('bien\n')
-        with open(fna, 'w') as out:
+
+        with open(files + '/' + fna, 'w') as out:
             out.write('>gi|'+gi[ind]+'|ref|'+seq.id+'|'+seq.description[len(seq.id):]+'\n'+seq.id)
 
     except ValueError:
-        print('ERROR: This search does not include the acc. number', fna[:-4])
+        print('ERROR: This search does not include the acc. number', fna)
