86c56bb0c45ed652c8eec3310998b18d4c39d730
valentina2095 <valentina2095@hotmail.com>
2017-12-08 04:50:01 -0500

    update db-download

--- db-download.py
+++ db-download.py
@@ -8,11 +8,26 @@ from Bio import SeqIO
 import datetime
 import time
 import glob
+import argparse
 
-Entrez.email = 'vsanche1@eafit.edu.co'
+parser = argparse.ArgumentParser(description='download specific database from\
+                                GenBank.')
+parser.add_argument('email', metavar='name@example.com', type=str, help='email\
+                    for NCBI Entrez tools')
+parser.add_argument('--org', metavar='ORGANISM', type=str, help='add organism\
+                    scientific name, Genbank common name or Taxonomy ID. (e.g.\
+                    Rodentia, 9989)')
+parser.add_argument('--strain', type=str, help='add the\
+                    strain to search for. (e.g. HUSEC2011)')
+parser.add_argument('--start', type=int, help='starting point for esearch,\
+                    Defaults to 0.')
 
-organism = 'Rodentia'
-strain = ''
+args = parser.parse_args()
+
+Entrez.email = args.email
+
+organism = args.org
+strain = args.strain
 keywords = ''
 search_phrase = ''
 
@@ -38,20 +53,23 @@ for index, n in enumerate(ncbi_values):
 
 print('\n Here is the complete search line that will be used: \n', search_phrase)
 
-start_ret = 15000+2101
-for i in range(1,5):
-    handle = Entrez.esearch(db='nuccore', term=search_phrase, retstart = start_ret,retmax=10000-2101, rettype='gbwithparts', retmode="text",idtype="acc")
+unprocessed = []
+
+rstart = args.start if args.start else 0
+rmax = 2000
+for i in range(5):
+    handle = Entrez.esearch(db='nuccore', term=search_phrase, retstart = rstart+i*rmax,retmax=rmax, rettype='gbwithparts', retmode="text",idtype="acc")
     print('\nThe esearch is complete')
 
     result = Entrez.read(handle, validate=False)
     handle.close()
     acc_numbers = result['IdList']
 
-    #########################
-    # Solo fasta por ahora
-    new_path = 'Genbank_Rodentia_Files/'
+    new_path = 'Genbank_Rodentia_casa/'
     if not os.path.exists(new_path):
         os.makedirs(new_path)
+    else:
+        previous = os.listdir(new_path)
 
     start_day = datetime.date.today().weekday() # 0 is Monday, 6 is Sunday
     start_time = datetime.datetime.now().time()
@@ -61,21 +79,32 @@ for i in range(1,5):
 
         auxiliar = 1
         for acc in acc_numbers:
-            if ((datetime.date.today().weekday() < 5 and datetime.datetime.now().time() > datetime.time(hour=21)) or
-                (datetime.date.today().weekday() < 5 and datetime.datetime.now().time() < datetime.time(hour=5)) or
-                (datetime.date.today().weekday() == start_day + 1 and datetime.datetime.now().time() < datetime.time(hour=5)) or
-                (datetime.date.today().weekday() >= 5) or len(acc_numbers) <= 100 ):
+            if ((datetime.date.today().weekday() < 5 and
+                datetime.datetime.now().time() > datetime.time(hour=21)) or
+                (datetime.date.today().weekday() < 5 and
+                datetime.datetime.now().time() < datetime.time(hour=5)) or
+                (datetime.date.today().weekday() == start_day + 1 and
+                datetime.datetime.now().time() < datetime.time(hour=5)) or
+                (datetime.date.today().weekday() >= 5) or len(acc_numbers) <=
+                100) and (not acc in previous):
 
-                print('\nDownloading file %s of %s   (%s' %(auxiliar, len(acc_numbers), 100*auxiliar/len(acc_numbers)) + '%)')
+                print('Downloading file %s of %s   (%s' %(auxiliar,
+                len(acc_numbers), 100*auxiliar/len(acc_numbers)) + '%)')
 
-                handle=Entrez.efetch(db='nuccore', rettype='gbwithparts', retmode='text', id=acc)
-                FILENAME =  new_path + acc + '.fna'
-                local_file=open(FILENAME,'w')
-                SeqIO.write(SeqIO.parse(handle, 'genbank'), local_file, "fasta")
-                handle.close()
-                local_file.close()
-                auxiliar += 1
+                try:
+                    handle=Entrez.efetch(db='nuccore', rettype='gbwithparts',
+                    retmode='text', id=acc)
+                    FILENAME =  new_path + acc + '.fna'
+                    local_file=open(FILENAME,'w')
+                    SeqIO.write(SeqIO.parse(handle, 'genbank'), local_file,
+                    "fasta")
+                    handle.close()
+                    local_file.close()
+                    auxiliar += 1
+                except:
+                    unprocessed.append(acc)
             else:
                 print('\nYou have too many files to download at the time. Try again later.')
-                print('The last accession we were able to download was %s, corresponding to %s of nuccore'  %s(acc, start_ret+auxiliar))
-    start_ret += 10000
+                print('The last accession we were able to download was %s, corresponding to %s of nuccore'  %(acc, rstart+auxiliar))
+
+print('unprocessed files:', unprocessed)
