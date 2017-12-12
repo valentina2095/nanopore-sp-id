c0a9cacb11b1b3013eb4b491b90efb6d895ec0a4
valentina2095 <vsanche1@eafit.edu.co>
2017-12-11 23:19:57 -0500

    update qbast

--- qblast.py
+++ qblast.py
@@ -16,38 +16,42 @@ parser.add_argument('input', type=str, help='input file, fasta and fastq\
                     formats are accepted.')
 parser.add_argument('-o', '--output', default='blast_record.xml', type=str,
                     help='desired output file name.')
-parser.add_argument('-e', '--expect', default=10, type=int, help='expect value\
-                    cutoff.')
+parser.add_argument('-e', '--expect', default=10, type=int,
+                    help='expect value cutoff.')
+parser.add_argument('--megablast', action='store_true',
+                    help='sum the integers (default: find the max)')
 #parser.add_argument('--blasthelp',)
 
 args = parser.parse_args()
-# print(args)
+print(args)
 
-if args.blasthelp:
-    help(NCBIWWW.qblast)
-
-# Define var filetype
-# https://stackoverflow.com/users/7802200/arya-mccarthy
-# def is_fasta(filename):
-#     with open(filename, "r") as handle:
-#         fasta = SeqIO.parse(handle, "fasta")
-#         return any(fasta)  # False when `fasta` is empty, i.e. wasn't a FASTA file
+# if args.blasthelp:
+#     help(NCBIWWW.qblast)
+#     # sys quit
 #
-# is_fasta(my_file)
-# # False
-
-read_file = open(args.input, 'r')
-reads = SeqIO.parse(read_file, filetype)
-
-out_handle = open(args.output, 'a')
-
-#for query in reads:
-    # result_handle = NCBIWWW.qblast('blastn', 'nt', query.seq, megablast=True, format_type='Text', expect=args.expect) # query can be a GI number, fasta file, sequence, SeqRecord
-    # saving the result handle
-#    out_handle.write(result_handle.read())
-#    result_handle.close()
-
-# parse output
-# result_handle = open('my_blast.xml','r')
-# blast_records = NCBIXML.parse(result_handle)
-# for blast_record in blast_records:
+# # Define var filetype
+# # https://stackoverflow.com/users/7802200/arya-mccarthy
+# def is_fasta(filename):
+#     with open(filename, 'r') as handle:
+#         fasta = SeqIO.parse(handle, 'fasta')
+#         return any(fasta)  # False when `fasta` is empty
+#
+# read_file = open(args.input, 'r')
+#
+#
+# if is_fastq():
+#
+#
+#
+# out_handle = open(args.output, 'a')
+#
+# #for query in reads:
+#     # result_handle = NCBIWWW.qblast('blastn', 'nt', query.seq, megablast=True, format_type='Text', expect=args.expect) # query can be a GI number, fasta file, sequence, SeqRecord
+#     # saving the result handle
+# #    out_handle.write(result_handle.read())
+# #    result_handle.close()
+#
+# # parse output
+# # result_handle = open('my_blast.xml','r')
+# # blast_records = NCBIXML.parse(result_handle)
+# # for blast_record in blast_records:
